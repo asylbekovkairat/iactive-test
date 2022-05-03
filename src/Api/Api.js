ee8b81ee32413bbdf63faaef2415bf62f5cd5c4f
unknown <asyl06kair@gmail.com>
2022-05-03 12:06:55 +0600

    fix bug with axios

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,22 +1,20 @@
-import axios from "axios"
+// import axios from "axios";
 
-export const url = "http://f0665380.xsph.ru"
+// const Api = axios.create({
+//   baseURL: "http://f0665380.xsph.ru",
+// // });
 
-const Api = axios.create({
-    baseURL: "http://f0665380.xsph.ru",
-});
+// Api.interceptors.request.use(
+//   function (config) {
+//     config.headers.messageId = 0;
+//     return config;
+//   },
+//   function (error) {
+//     console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
 
-Api.interceptors.request.use(
-    function(config){
-        config.headers.messageId = 0
-        return config;
-    },
-    function(error){
-        console.log(error);
-        return Promise.reject(error);
-    }
-);
-
-export default{
-    getFirstMessages: () => Api.post(),
-}
+// export default {
+//   getFirstMessages: () => Api.post(),
+// };
